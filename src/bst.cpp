7572c6d1cd1c9d3016931c74f0c75b73d62bfa8c
glay_luncy <ie925155@gmail.com>
2023-12-19 23:19:36 +0800

    skeleton code

--- src/bst.cpp
+++ src/bst.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+
 #include "bst.h"
+
+Node*& BST::get_root()
+{
+    return this->root;
+}
+
+
+void BST::bfs(std::function<void(Node*& node)> func)
+{
+
+}
+
+size_t BST::length()
+{
+    return 0;
+}
+
+bool BST::add_node(int value)
+{
+    return true;
+}
+
+Node** BST::find_node(int value)
+{
+    return NULL;
+}
+Node** BST::find_parrent(int value)
+{
+    return NULL;
+}
+Node** find_successor(int value)
+{
+    return NULL;
+}
+
+bool delete_node(int value)
+{
+    return NULL;
+}
